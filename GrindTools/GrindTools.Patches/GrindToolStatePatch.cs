fca59f7ab4bc3a79bb757fd7ecb3d065c7bbad7a
GuruXL <dillon.cordiner@gmail.com>
2023-10-15 16:24:25 -0400

    added GrindToolStatePatch for scaling spline tool when using capsule colliders

--- GrindTools/GrindTools.Patches/GrindToolStatePatch.cs
+++ GrindTools/GrindTools.Patches/GrindToolStatePatch.cs
@@ -3,21 +3,36 @@ using MapEditor;
 using Object = UnityEngine.Object;
 using System.Reflection;
 using System;
+using UnityEngine;
 
 namespace GrindTools.Patches
 {
     [HarmonyPatch(typeof(GrindSplineToolState), "Update")]
     public static class GrindToolStatePatch
     {
-        [HarmonyPostfix]
-        static bool Postfix(GrindSplineToolState __instance)
-        {   
-            return true; // Execute the original method
+        [HarmonyPrefix]
+        static void Prefix(GrindSplineToolState __instance)
+        {
+            if (!Main.settings.capColliders)
+                return;
+
+            UpdateScale(ref __instance.width, ref __instance.height);
+            AccessTools.Method(typeof(GrindSplineToolState), "UpdateSizes").Invoke(__instance, new object[] { __instance.width, __instance.height });
+        }
+
+        private static void UpdateScale(ref float width, ref float height)  // Parameters by reference
+        {
+            if (width != height)
+            {
+                width = height;
+            }
+            else if (height != width)
+            {
+                height = width;
+            }
         }
     }
 }
-
-
 
 
 
